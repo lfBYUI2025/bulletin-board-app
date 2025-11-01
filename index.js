6ab936b3b22aca959f5c29e868f1b5c627eec969
lfBYUI2025 <fli21003@byui.edu>
2025-11-01 11:24:29 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+const PORT = 8080;
+http.createServer((req, res) => {
+  if (req.url === '/' || req.url === '/index.html') {
+    fs.readFile(path.join(__dirname, 'public', 'index.html'), (err, data) => {
+      if (err) { res.writeHead(500); return res.end('Error'); }
+      res.writeHead(200, { 'Content-Type': 'text/html' }); res.end(data);
+    });
+  } else { res.writeHead(404); res.end('Not found'); }
+}).listen(PORT, () => console.log('App on ' + PORT));
